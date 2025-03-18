d9212e63d8e8b1b95e8a6ca74e3f0ab4cc1d67e5
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-03-18 17:42:35 +0000

    implemented helmet without configurations

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import helmet from "helmet";
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
 
 const app: Express = express();
 app.use(express.json());
+
+app.use(helmet());
 
 /**
  * Mount moderation routes on /api/v1/moderation
